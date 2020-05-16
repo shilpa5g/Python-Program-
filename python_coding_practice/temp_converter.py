680c3bc3e6815f2f59012d9caa4631f13bd0f9fa
Shilpa Kumari <gshilpa690@gmail.com>
2020-05-16 15:41:23 +0530

    convert celcius to fahrenheit program

---
+++ python_coding_practice/temp_converter.py
@@ -0,0 +1,7 @@
+# program to convert temperature in celsius to fahrenheit
+temp_celcius = float(input("enter the value of temperature in celsius: "))
+print("  ")
+
+temp_fahrenheit = (temp_celcius * 1.8) + 32
+
+print(temp_celcius,"degree celsius =",temp_fahrenheit, "degree fahrenheit.")
