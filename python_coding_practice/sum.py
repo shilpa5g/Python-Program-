b2271d8219b10cb2a690f061d31251eb679060e6
Shilpa Kumari <gshilpa690@gmail.com>
2020-05-16 10:26:31 +0530

    program to add two numbers

---
+++ python_coding_practice/sum.py
@@ -0,0 +1,6 @@
+# program for add two numbers
+num1 = int(input("enter the first number: "))
+num2 = int(input("enter the second number"))
+
+sum = num1 + num2
+print("the sum of {} and {} is {}." .format(num1, num2, sum))
