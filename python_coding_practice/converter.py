ed892bcfb6a33110ccc1e1d9b432e41884077642
Shilpa Kumari <gshilpa690@gmail.com>
2020-05-17 09:25:38 +0530

    program to convert decimal number in binary, hexadecimal and octal.

---
+++ python_coding_practice/converter.py
@@ -0,0 +1,9 @@
+dec = int(input("enter a decimal value: "))
+
+print(" decimal value of",dec, "equal to :")
+
+print(bin(dec),"in binary.")
+
+print(hex(dec), "in hexadecimal.")
+
+print(oct(dec),"in octal.")
