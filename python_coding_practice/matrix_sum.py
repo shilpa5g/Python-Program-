091592b28f525520d321a7bbbfbf2068024fa216
Shilpa Kumari <gshilpa690@gmail.com>
2020-05-17 16:39:32 +0530

    program to find sum of two matrices

---
+++ python_coding_practice/matrix_sum.py
@@ -0,0 +1,23 @@
+
+x =  [[1, 1, 6],
+     [3, 5, 8],
+     [4, 6, 9]]
+
+y =  [[2,  5, 9],
+     [12, 34, 6],
+     [13, 6, 8]]
+
+
+result =  [[0, 0, 0],
+          [0, 0, 0],
+          [0, 0, 0]]
+
+for i in range(len(x)):
+    for j in range(len(x[0])):
+
+     	result[i][j] = x[i][j] + y[i][j]
+
+for r in result:
+    print(r)
+
+
