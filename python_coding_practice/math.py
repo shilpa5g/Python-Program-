1b63175be63f5e2c05e32df553a57df7baa8cc43
Shilpa Kumari <gshilpa690@gmail.com>
2020-05-17 20:07:17 +0530

    program to perform different set operations

---
+++ python_coding_practice/math.py
@@ -0,0 +1,14 @@
+# Program to perform different set operations like in mathematics
+
+set1 = {10, 11, 12, 13, 16};
+set2 = {11, 12, 13, 14, 15};
+print("set1 = {10, 11, 12, 13, 16}\n")
+print("et2 = {11, 12, 13, 14, 15}\n")
+
+print("Union of set1 and set2 is",set1 | set2, '\n\n')
+
+print("Intersection of set1 and set2 is",set1 & set2,'\n\n')
+
+print("Difference of set1 and set2 is",set1 - set2,'\n\n')
+
+print("Symmetric difference of set1 and set2 is",set1 ^ set2,'\n\n')
