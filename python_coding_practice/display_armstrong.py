43eec12c3a366fbfff1c4c60d43432daa2cd3e98
Shilpa Kumari <gshilpa690@gmail.com>
2020-05-17 08:23:30 +0530

    program to display armstrong number

---
+++ python_coding_practice/display_armstrong.py
@@ -0,0 +1,19 @@
+# Program to display Armstrong numbers within an interval
+
+lower = 1
+upper = 1000
+
+print("armstrong number between interval",(lower, upper),":")
+for num in range(lower, upper + 1):
+
+   
+   sum = 0
+
+   temp = num
+   while temp > 0:
+       digit = temp % 10
+       sum += digit ** 3
+       temp //= 10
+
+   if num == sum:
+       print(num)
