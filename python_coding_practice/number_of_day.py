97e7b208daefefdf51fc395a6cd935d48d2adaac
Shilpa Kumari <gshilpa690@gmail.com>
2020-05-22 08:39:53 +0530

    program to count total days between two dates

---
+++ python_coding_practice/number_of_day.py
@@ -0,0 +1,6 @@
+from datetime import date
+firstday = date(2020, 9, 5)
+lastday = date(2021, 9, 5)
+
+number_of_day = lastday - firstday
+print("the number of day between",firstday,'and',lastday,'are: ',number_of_day)
