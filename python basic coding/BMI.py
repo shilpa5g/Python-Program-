3f4db69b3ef389fd5522f8fcb879d65d8defa2a3
Shilpa Kumari <gshilpa690@gmail.com>
2020-05-26 23:16:55 +0530

    program to find body mass index

---
+++ python basic coding/BMI.py
@@ -0,0 +1,4 @@
+height = float(input("Input height in meters: "))
+weight = float(input("Input weight in kilogram: "))
+BMI = weight / (height * height)
+print("Your body mass index is: ",round(BMI,3))
