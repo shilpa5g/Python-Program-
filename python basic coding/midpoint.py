fc767c1cfb4070b291684ae3e76a471cf2634778
Shilpa Kumari <gshilpa690@gmail.com>
2020-05-27 10:58:53 +0530

    program to find mid point of a line

---
+++ python basic coding/midpoint.py
@@ -0,0 +1,7 @@
+x1 = int(input('value of x1: '))
+y1 = int(input('value of 11: '))
+x2 = int(input('value of x2: '))
+y2 = int(input('value of y2: '))
+mid_x = (x1 + x2)/2
+mid_y = (y1 + y2)/2
+print("mid point of the line is: ",mid_x,',',mid_y)
